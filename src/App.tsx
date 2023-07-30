9c603d7341a33a8cc941c6e11aa3918e2b500763
Yogeshwaran R <yogeshwaran08083@gmail.com>
2023-07-30 12:57:40 +0530

    Basic ops implemented todo list 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.tsx 	new file:   src/assets/delete.png 	new file:   src/assets/delete.svg 	new file:   src/assets/icons8-edit-100.png 	new file:   src/assets/tick.png 	new file:   src/assets/to-do-list.png 	new file:   src/assets/todo-icon.png 	deleted:    src/index.css 	modified:   src/index.tsx 	deleted:    src/logo.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface TodoList {
+  id : number;
+  task : string;
+  completed : boolean;
+}
+
 function App() {
+
+  const [task, setTask] = useState<string>("");
+  const [completed, isCompleted] = useState<boolean>(false);
+  const [todos, setTodos] = useState<TodoList[]>([]);
+
+  const handleOnChange =  (e:React.ChangeEvent<HTMLInputElement>) => {
+      setTask(e.target.value);
+  }
+
+  const handleAddTask = (e:React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
+      if (task.trim() != ''){
+        const taskObj:TodoList = {
+          "id" : Date.now(),
+          "task" : task,
+          "completed" : false,
+        };
+        setTodos([...todos, taskObj]);
+        setTask("")
+      }
+  }
+
+  const handleOnCheckChange = (index:number) => {
+    const updatedTodos = todos.map((todo) => 
+    todo.id === index?{...todo, completed : !todo.completed} : todo);
+    setTodos(updatedTodos);
+    console.log(todos);
+  }
+
+  const handleDeleteTask = (index:number) =>{
+    const updatedTodos = todos.filter((todo) => todo.id != index);
+    setTodos(updatedTodos);
+  }
+
+  const completeAllTasks = () => {
+    setTodos(() => todos.map((todo) => ({...todo,completed:true})));
+  }
+
+  const deleteCompletedTasks = () => {
+    setTodos(() => todos.filter((todo) => todo.completed !== true))
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        
+        <div className='header'>
+          <img src={require("./assets/to-do-list.png")} id="todoIcon"></img>
+          <h1>Todo List</h1>
+        </div>
+
+        <form className="formInput">
+          <input type="text" 
+          placeholder='Enter the task...'
+          className='inputField'
+          value={task}
+          onChange={handleOnChange}/>
+          <button className='btn' onClick={handleAddTask}>Add</button>
+        </form>
+
+        <div className='actionBar'>
+          <div className='actionSection' onClick={completeAllTasks}>
+            <img src={require("./assets/tick.png")} className='actionIcon'/>
+            <p className='actionLabel'>Complete all tasks</p>       
+          </div>
+
+          <div className='actionSection' onClick={deleteCompletedTasks}>
+            <img src={require("./assets/delete.png")} className='actionIcon'/>
+            <p className='actionLabel'>Delete Comp Task</p>
+          </div>
+        </div>
+
+        <div className='taskContainer'>
+            {todos.map((t, index) => (
+
+            <div className='taskList'>
+              <div style={{display:'flex',alignItems:'center'}}>
+                <input type='checkbox'  onChange={() => handleOnCheckChange(t.id)} className='chkBx' checked={t.completed}/>
+                <label>{t.task}</label>
+              </div>
+
+              <div className='chkTile'>
+                <img src={require("./assets/edit.png")} className='taskIcon' />
+                <img src={require("./assets/delete.png")} className='taskIcon' onClick={() => handleDeleteTask(t.id)}/>
+              </div>
+            </div>))}
+            
+        </div>
     </div>
   );
 }
